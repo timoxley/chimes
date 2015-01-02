c43fe057ff9be74c783bba120e36e2fe3fbfce37
Tim Oxley <secoif@gmail.com>
2015-01-02 21:41:26 +1000

    Update generators test.

--- test/generators.js
+++ test/generators.js
@@ -3,17 +3,14 @@
 const test = require('tape')
 const {Iterable} = require('./common')
 
-const {every, value} = require('../')
-
-
+const {map, value} = require('../')
 
 test('map with generator', t => {
   let size = 3
   let nums = map(function* () {
-    while(--size) yield size
+    while(size--) yield size
   }, num => num * 2)
 
-  let set = new Set(asyncSeries)
-  t.deepEqual(Array.from(nums), [1,2,3].map(n => n * 2))
+  t.deepEqual(Array.from(nums), [2,1,0].map(n => n * 2))
   t.end()
 })
